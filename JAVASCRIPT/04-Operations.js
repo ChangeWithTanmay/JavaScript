fc0f0dd90506b009e1ee01c1295623369e7ed0b0
ChangeWithtanmay <tanmaydas24pgs@gmail.com>
2024-03-17 19:39:11 +0530

    Add Operator and Comparison file

---
+++ JAVASCRIPT/04-Operations.js
@@ -0,0 +1,32 @@
+// ***************** Operations *************
+
+let value =3
+let negvalue=-value
+
+console.log(negvalue)
+
+// Basic Operation 
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3**2)
+console.log(3/2)
+console.log(3%2)
+
+
+// ******* String Concatenation *******
+
+let str1="Tanmay"
+let str2=" Das"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2) // => 122
+console.log(1+2+"2") // => 32
+
+console.log(true);
+console.log(+true); // => 1
+console.log(+"");  // => 0
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <p> frist commment.. </p>
+<br>
+<h1>Now we are working in JavaScript Learnning</h1>

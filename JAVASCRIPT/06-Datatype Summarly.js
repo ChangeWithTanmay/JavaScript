599d210e77c5f0eda18ae85638fc80c9bc567eee
ChangeWithtanmay <tanmaydas24pgs@gmail.com>
2024-04-07 09:17:58 +0530

    Add Datatype and memory

---
+++ JAVASCRIPT/06-Datatype Summarly.js
@@ -0,0 +1,39 @@
+// primitive
+
+//  7 Types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const Name="Tanmay"
+const Num=123
+const Num1=123.099
+
+let va=true
+let Bol=null;
+let userMail;
+
+const id=Symbol('123')
+const AnothorId=Symbol('123')
+
+console.log(id === AnothorId); // False
+
+const bigNumber = 12345678945678456n;
+
+//  Reference (Non Primitive)
+
+//  Array, Objects, Functions
+
+// Arrays
+let heros=["Antman","Iron man", "Captain Amarica"];
+
+// Objects
+let myObj={
+    name:"Tanmay Das",
+    age:20,
+
+}
+
+// Functions
+
+let myfunction=function(){
+    console.log("Hello wrold");
+}
+
---
+++ JAVASCRIPT/07-Memory js.js
@@ -0,0 +1,11 @@
+// Memory Two type 
+
+// Stack Memory  (Primitive), Heap Memory (Non-Primitive)
+
+let myStartupname="Vectosmind"
+
+let anothername=myStartupname
+
+anothername="triprivo"
+console.log(anothername);
+console.log(myStartupname);

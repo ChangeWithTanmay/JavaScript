b92dfbbce13c923b4959aa96a37f8ae120ae9d5c
ChangeWithtanmay <tanmaydas24pgs@gmail.com>
2024-04-07 09:40:26 +0530

    In memory file samething addd

--- JAVASCRIPT/07-Memory js.js
+++ JAVASCRIPT/07-Memory js.js
@@ -2,6 +2,7 @@
 
 // Stack Memory  (Primitive), Heap Memory (Non-Primitive)
 
+// Stack memory
 let myStartupname="Vectosmind"
 
 let anothername=myStartupname
@@ -9,3 +10,12 @@ let anothername=myStartupname
 anothername="triprivo"
 console.log(anothername);
 console.log(myStartupname);
+
+let user={
+    email: "user@gmail.com", // reference
+    upi:"user@ybl"
+}
+
+let user1=user;
+
+console.log(user1)

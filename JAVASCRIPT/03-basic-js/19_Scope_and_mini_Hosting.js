6d717cdc800a7a0e0275b536c260fd02ecc6ebca
ChangeWithtanmay <tanmaydas24pgs@gmail.com>
2024-04-16 18:23:47 +0530

    add 19 No file that is are 19_scope_and_minihosting.js

---
+++ JAVASCRIPT/03-basic-js/19_Scope_and_mini_Hosting.js
@@ -0,0 +1,34 @@
+function one(){
+    const username="Tanmay"
+
+    function two(){
+        const website="Youtube"
+        console.log(username);
+    }
+    two() // Function call
+}
+
+// one()  // Function call
+
+if (true) {
+    const username="Tanmay"
+    if (username=="Tanmay") {
+        const website=" Youtube"
+        // console.log(username+website);
+    }
+    
+
+}
+
+
+// ++++++++++++ Interesting ++++++++
+
+function addone(num) {
+    return num+1;
+}
+addone(5)
+
+const addtwo=function (num) {
+    return num+2;
+}
+addtwo(5)

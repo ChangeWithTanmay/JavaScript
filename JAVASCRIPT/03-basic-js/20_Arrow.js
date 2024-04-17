ab8d9c16a0b8272b29029cadb2fb7cd46f066c42
ChangeWithtanmay <tanmaydas24pgs@gmail.com>
2024-04-17 08:48:44 +0530

    Add 20 no file in js Serice

---
+++ JAVASCRIPT/03-basic-js/20_Arrow.js
@@ -0,0 +1,62 @@
+// This --> current contex refer..
+
+const user={
+    username: "Tanmay",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, Welcome to website.`)
+        console.log(this)
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+
+
+// console.log(this)
+
+// function chai(){
+//     let username="Tanmay";
+//     console.log(this.username) // This never use in Function..
+// }
+
+// chai()
+
+
+
+// const chai=function(){
+//     let username="Tanmay"
+//     console.log(this.username)
+// }
+
+
+// chai()
+
+/// ++++++++ Arraow Function ++++++++
+
+const chai= () =>{
+    let username="Tanmay"
+    console.log(this.username)
+}
+
+
+
+
+// const addTwo = (num1, num2) =>{   // bassic method
+//     return num1+num2;
+// }
+
+// const addTwo = (num1, num2) =>num1+num2;  // Advance Method
+// const addTwo = (num1, num2) =>(num1+num2);  // Advance Method
+
+
+// ++++++ Arrow Function Object +++++
+
+const addTwo = (num1, num2) =>({username: "Tanmay"});  // Advance Method
+
+
+
+console.log(addTwo(3,4))

b609689a1923ab459cb02ee1ceedf2c99f2fb85c
ChangeWithtanmay <tanmaydas24pgs@gmail.com>
2024-04-29 21:53:16 +0530

    add 06.js file

---
+++ JAVASCRIPT/04_Control_Flow/06.js
@@ -0,0 +1,15 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values=coding.forEach( (item) => {
+
+//     console.log(item)
+    
+// } )
+
+// console.log(values) // No Return 
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+const newNumbs = myNums.filter( (num) => num >4)
+console.log(newNumbs)
